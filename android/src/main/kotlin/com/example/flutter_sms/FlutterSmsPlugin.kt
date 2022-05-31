2405e24618fb24aa6d0908e660a6414cf8c57f2f
trietlam <lxtriet@gmail.com>
2022-05-31 15:37:40 +0700

    Change business notify flutter after move back from sms screen - only android

--- android/src/main/kotlin/com/example/flutter_sms/FlutterSmsPlugin.kt
+++ android/src/main/kotlin/com/example/flutter_sms/FlutterSmsPlugin.kt
@@ -1,10 +1,10 @@
 package com.example.flutter_sms
-
 import android.annotation.TargetApi
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry
 import io.flutter.plugin.common.PluginRegistry.Registrar
 import android.app.Activity
 import android.net.Uri
@@ -16,45 +16,37 @@ import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.BinaryMessenger
-
-class FlutterSmsPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
+class FlutterSmsPlugin: FlutterPlugin, MethodCallHandler, ActivityAware, PluginRegistry.ActivityResultListener {
   private lateinit var mChannel: MethodChannel
   private var activity: Activity? = null
+  private var sendSMSResult: Result? = null
   private val REQUEST_CODE_SEND_SMS = 205
-
   override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    binding.addActivityResultListener(this)
     activity = binding.activity
   }
-
   override fun onDetachedFromActivity() {
     activity = null
   }
-
   override fun onDetachedFromActivityForConfigChanges() {
     activity = null
   }
-
   override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
     activity = binding.activity
   }
-
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     setupCallbackChannels(flutterPluginBinding.binaryMessenger)
   }
-
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     teardown()
   }
-
   private fun setupCallbackChannels(messenger: BinaryMessenger) {
     mChannel = MethodChannel(messenger, "flutter_sms")
     mChannel.setMethodCallHandler(this)
   }
-
   private fun teardown() {
     mChannel.setMethodCallHandler(null)
   }
-
   // V1 embedding entry point. This is deprecated and will be removed in a future Flutter
   // release but we leave it here in case someone's app does not utilize the V2 embedding yet.
   companion object {
@@ -65,7 +57,6 @@ class FlutterSmsPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
       inst.setupCallbackChannels(registrar.messenger())
     }
   }
-
   override fun onMethodCall(call: MethodCall, result: Result) {
     when (call.method) {
         "sendSMS" -> {
@@ -78,13 +69,13 @@ class FlutterSmsPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
           }
           val message = call.argument<String?>("message")
           val recipients = call.argument<String?>("recipients")
-          sendSMS(result, recipients, message!!)
+          sendSMSResult = result
+          sendSMS(recipients, message!!)
         }
         "canSendSMS" -> result.success(canSendSMS())
         else -> result.notImplemented()
     }
   }
-
   @TargetApi(Build.VERSION_CODES.ECLAIR)
   private fun canSendSMS(): Boolean {
     if (!activity!!.packageManager.hasSystemFeature(PackageManager.FEATURE_TELEPHONY))
@@ -94,13 +85,17 @@ class FlutterSmsPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
     val activityInfo = intent.resolveActivityInfo(activity!!.packageManager, intent.flags.toInt())
     return !(activityInfo == null || !activityInfo.exported)
   }
-
-  private fun sendSMS(result: Result, phones: String?, message: String?) {
+  private fun sendSMS(phones: String?, message: String?) {
     val intent = Intent(Intent.ACTION_SENDTO)
     intent.data = Uri.parse("smsto:$phones")
     intent.putExtra("sms_body", message)
     intent.putExtra(Intent.EXTRA_TEXT, message)
     activity?.startActivityForResult(intent, REQUEST_CODE_SEND_SMS)
-    result.success("SMS Sent!")
+  }
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
+    when (requestCode) {
+      REQUEST_CODE_SEND_SMS -> sendSMSResult?.success("SMS Sent!")
+    }
+    return false
   }
 }
